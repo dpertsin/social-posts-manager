5342d5786c8fd6764a4db98345185ad9e15607fd
Dimitrios Pertsinidis <dpertsin@gmail.com>
2025-02-20 16:40:45 +0200

    no need index.css because of CssBaseline

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Routes from "./routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import { CssBaseline } from "@mui/material";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
